a3dd4f3adbba535d8ed54a66656cf9ddb527f05d
JohnB <jbryantmatthews@gmail.com>
2019-02-01 06:51:59 -0500

    saving

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
-  port: 3000,
+  port: 3306,
 
   // Your username
   user: "root",
@@ -28,22 +28,16 @@ connection.connect(function (err) {
   display();
   // start();
 });
-
 function display() {
 
   connection.query(function (err, res) {
 
     if (err) throw err;
-
-    var table = new Table({
-      head: ['ID', 'Product Name', 'Department', 'Price', 'Stock Quantity']
-  });
-
     for (var i = 0; i < res.length; i++) {
-      table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+      table.push(res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity);
     }
+    
+   
   });
-
-
 }
 
