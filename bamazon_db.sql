a469187a789ca4ccdd20e608695796036aadc776
JohnB <jbryantmatthews@gmail.com>
2019-02-01 06:31:40 -0500

    saving

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -1,78 +1,78 @@
-DROP DATABASE IF EXISTS starwars_db;
-CREATE DATABASE starwars_db;
-USE starwars_db;
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+USE bamazon_db;
 
 -- Create the products table
 CREATE TABLE products
 (
   id INT NOT NULL AUTO_INCREMENT,
   item_id INT NOT NULL,
-  product_name VARCHAR (255) NOT NULL,
-  department_name VARCHAR NOT NULL,
+  name VARCHAR (255) NULL,
+  department_name VARCHAR (255) NULL,
   price INT NOT NULL,
   stock_quantity INT NOT NULL,
   PRIMARY KEY(id)
 );
 
 
-USE starwars_db;
+USE bamazon_db;
 
 INSERT INTO products
-  (item_id, product_name, department_name, price, stock_quantity)
+  (item_id, name, department_name, price, stock_quantity)
 VALUES
-  (123456789, 'Soul Eater', 'AnimeDVD', 18, 2000 );
+  (001, "Soul Eater", "AnimeDVD", 18, 2000 );
 
   INSERT INTO products
-  (item_id, product_name, department_name, price, stock_quantity)
+  (item_id, name, department_name, price, stock_quantity)
 VALUES
-  (112345678, 'Akira', 'AnimeDVD', 14, 2000 );
+  (002, "Akira", "AnimeDVD", 14, 2000 );
 
 INSERT INTO products
-  (item_id, product_name, department_name, price, stock_quantity)
+  (item_id, name, department_name, price, stock_quantity)
 VALUES
-  (112345678, 'Bad Boys', 'ActionDVD', 14, 2000 );
+  (003, "Bad Boys", "ActionDVD", 14, 2000 );
 
 INSERT INTO products
-  (item_id, product_name, department_name, price, stock_quantity)
+  (item_id, name, department_name, price, stock_quantity)
 VALUES
-  (112345678, 'Reign of the Supermen', 'ActionDVD', 14, 2000 );
+  (004, "Reign of the Supermen", "ActionDVD", 14, 2000 );
 
   INSERT INTO products
-  (item_id, product_name, department_name, price, stock_quantity)
+  (item_id, name, department_name, price, stock_quantity)
 VALUES
-  (6307174, 'The Nutcracker and the Four Realms', 'ActionDVD', 16, 2000 );
+  (005, "The Nutcracker and the Four Realms", "ActionDVD", 16, 2000 );
 
   INSERT INTO products
-  (item_id, product_name, department_name, price, stock_quantity)
+  (item_id, name, department_name, price, stock_quantity)
 VALUES
-  (6315008, 'Reign of the Supermen', 'ActionDVD', 14, 2000 );
+  (006, "Reign of the Supermen", "ActionDVD", 14, 2000 );
 
   INSERT INTO products
-  (item_id, product_name, department_name, price, stock_quantity)
+  (item_id, name, department_name, price, stock_quantity)
 VALUES
-  (112345678, 'Jumanji: Welcome to the Jungle', ' FantasyDVD', 14, 2000 );
+  (007, "Jumanji: Welcome to the Jungle", " FantasyDVD", 14, 2000 );
 
   INSERT INTO products
-  (item_id, product_name, department_name, price, stock_quantity)
+  (item_id, name, department_name, price, stock_quantity)
 VALUES
-  (112345678, 'Halloween', 'HorrorDVD', 14, 2000 );
+  (008, "Halloween", "HorrorDVD", 14, 2000 );
 
   INSERT INTO products
-  (item_id, product_name, department_name, price, stock_quantity)
+  (item_id, name, department_name, price, stock_quantity)
 VALUES
-  (112345678, 'Oceans 8', 'ThrillerDVD', 14, 2000 );
+  (009, "Oceans 8", "ThrillerDVD", 14, 2000 );
 
   INSERT INTO products
-  (item_id, product_name, department_name, price, stock_quantity)
+  (item_id, name, department_name, price, stock_quantity)
 VALUES
-  (112345678, 'Split', 'ThrillerDVD', 14, 2000 );
+  (010, "Split", "ThrillerDVD", 14, 2000 );
 
   INSERT INTO products
-  (item_id, product_name, department_name, price, stock_quantity)
+  (item_id, name, department_name, price, stock_quantity)
 VALUES
-  (112345678, 'Fury', 'ActionDVD', 14, 2000 );
+  (011, "Fury", "ActionDVD", 14, 2000 );
 
   INSERT INTO products
-  (item_id, product_name, department_name, price, stock_quantity)
+  (item_id, name, department_name, price, stock_quantity)
 VALUES
-  (112345678, 'Akira', 'AnimeDVD', 14, 2000 );
+  (012, "Akira", "AnimeDVD", 14, 2000 );
